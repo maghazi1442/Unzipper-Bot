0fa64c797beb0fb770a297b51e0bf64d5b0df6bd
maghazi1442 <83565356+maghazi1442@users.noreply.github.com>
2021-10-14 23:27:36 +0700

    Update up_helper.py

--- unzipper/modules/ext_script/up_helper.py
+++ unzipper/modules/ext_script/up_helper.py
@@ -11,12 +11,12 @@ from pyrogram.errors import FloodWait
 # Send file to a user
 async def send_file(unzip_bot, c_id, doc_f, query, full_path):
     try:
-        await unzip_bot.send_document(chat_id=c_id, document=doc_f, caption="**Extracted by @NexaUnzipper_Bot**")
+        await unzip_bot.send_document(chat_id=c_id, document=doc_f, caption="**Di ekstrak oleh #Annajiyah_Media_Center**")
         os.remove(doc_f)
     except FloodWait as f:
         asyncio.sleep(f.x)
         return send_file(c_id, doc_f)
     except FileNotFoundError:
-        await query.answer("Sorry! I can't find that file", show_alert=True)
+        await query.answer("Maaf kami tidak menemukan file ini", show_alert=True)
     except BaseException:
         shutil.rmtree(full_path)
