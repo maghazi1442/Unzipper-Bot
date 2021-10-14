7a0a8cb3fbc2e0fbf379761f66ed05444577fdf9
maghazi1442 <83565356+maghazi1442@users.noreply.github.com>
2021-10-15 02:13:42 +0700

    Update callbacks.py

--- unzipper/modules/callbacks.py
+++ unzipper/modules/callbacks.py
@@ -42,16 +42,16 @@ async def unzipper_cb(unzip_bot: Client, query: CallbackQuery):
                 url = r_message.text
                 # Double check
                 if not re.match(https_url_regex, url):
-                    return await query.message.edit("`That's not a valid url 😑!`")
+                    return await query.message.edit("`Link tidak valid`")
                 s = ClientSession()
                 async with s as ses:
                     unzip_head = await ses.head(url)
                     # Checks if url file size is bigger than 2GB (Telegram limit)
                     u_file_size = unzip_head.headers.get('content-length')
                     if u_file_size is None:
-                        return await query.message.edit("`Sorry, An Error occurred while getting file size. Please try again after some time 🥺!`")
+                        return await query.message.edit("`Maaf, Terjadi Kesalahan saat mendapatkan ukuran file. Silakan coba lagi setelah beberapa saat`")
                     if Config.TG_MAX_SIZE < int(u_file_size):
-                        return await query.message.edit("`File Size is too large to send in telegram 🥶!`")
+                        return await query.message.edit("`Ukuran File terlalu besar untuk dikirim di telegram`")
                     unzip_resp = await ses.get(url)
                     # Checks if file is an archive using content-type header
                     if "application/" not in unzip_resp.headers.get('content-type'):
@@ -63,19 +63,19 @@ async def unzipper_cb(unzip_bot: Client, query: CallbackQuery):
                         await unzip_bot.send_message(chat_id=Config.LOGS_CHANNEL, text=Messages.LOG_TXT.format(user_id, url, u_file_size))
                         s_time = time()
                         u_file_with_ext = f"{download_path}/archive_from_{user_id}{os.path.splitext(url)[1]}"
-                        await query.message.edit(f"**Trying to download!** \n\n**Url:** `{url}` \n\n`This may take a while, Go and grab a coffee ☕️!`")
+                        await query.message.edit(f"**Mencoba untuk mendownload** \n\n**Url:** `{url}` \n\n`Ini mungkin butuh waktu lama, harap tunggu dan bersabar`")
                         file = await aiofiles.open(u_file_with_ext, mode="wb")
                         await file.write(await unzip_resp.read())
                         await file.close()
                         archive = u_file_with_ext
                         e_time = time()
                     else:
-                        await query.message.edit("**Sorry I can't download that URL 🥺!**")
+                        await query.message.edit("**Maaf, saya tidak bisa mendownload dari link ini!**")
                         return
             
             elif splitted_data[1] == "tg_file":
                 if r_message.document is None:
-                    return await query.message.edit("`Give me an Archive to extract lamo!`")
+                    return await query.message.edit("`Berikan saya untuk mengekstrak`")
                 # Makes download dir
                 os.makedirs(download_path)
                 # Send Logs
@@ -84,21 +84,21 @@ async def unzipper_cb(unzip_bot: Client, query: CallbackQuery):
                 s_time = time()
                 archive = await r_message.download(
                     file_name=f"{download_path}/archive_from_{user_id}{os.path.splitext(r_message.document.file_name)[1]}",
-                    progress=progress_for_pyrogram, progress_args=("**Trying to Download!** \n", query.message, s_time)
+                    progress=progress_for_pyrogram, progress_args=("**Mencoba untuk mendownload!** \n", query.message, s_time)
                     )
                 e_time = time()
             else:
-                await query.answer("Can't Find Details! Please contact support group!", show_alert=True)
+                await query.answer("Tidak bisa menemukan dengan detail", show_alert=True)
             
             try:
                 await query.message.edit(Messages.AFTER_OK_DL_TXT.format(TimeFormatter(round(e_time-s_time) * 1000)))
             except:
-                await query.answer("Successfully Downloaded! Extracting Now 😊!", show_alert=True)
+                await query.answer("Berhasil diunduh! Mengekstraks sekarang", show_alert=True)
             
 
 
             if splitted_data[2] == "with_pass":
-                password = await unzip_bot.ask(chat_id=query.message.chat.id ,text="**Please send me the password 🔑:**")
+                password = await unzip_bot.ask(chat_id=query.message.chat.id ,text="**kirimkan paswordnya:**")
                 ext_s_time = time()
                 extractor = await extr_files(path=ext_files_dir, archive_path=archive, password=password.text)
                 ext_e_time = time()
@@ -116,7 +116,7 @@ async def unzipper_cb(unzip_bot: Client, query: CallbackQuery):
             # Upload extracted files
             paths = get_files(path=ext_files_dir)
             i_e_buttons = await make_keyboard(paths=paths, user_id=user_id, chat_id=query.message.chat.id)
-            await query.message.edit("Select Files to Upload!", reply_markup=i_e_buttons)
+            await query.message.edit("Pilih file untuk di upload", reply_markup=i_e_buttons)
         except Exception as e:
             try:
                 await query.message.edit(Messages.ERROR_TXT.format(e))
@@ -133,9 +133,9 @@ async def unzipper_cb(unzip_bot: Client, query: CallbackQuery):
         if not paths:
             if os.path.isdir(f"{Config.DOWNLOAD_LOCATION}/{spl_data[1]}"):
                 shutil.rmtree(f"{Config.DOWNLOAD_LOCATION}/{spl_data[1]}")
-            return await query.message.edit("`I've already sent you those files 😐, Don't ask me to resend 😒!`")
+            return await query.message.edit("`Sayasudah mengirimimu file-file itu, Jangan minta saya kirim ulang`")
         
-        await query.answer("Send that file to you. Please wait!")
+        await query.answer("saya mengirimi Anda file, tunggu dan sabar")
         await send_file(unzip_bot=unzip_bot,
                         c_id=spl_data[2],
                         doc_f=paths[int(spl_data[3])],
@@ -144,7 +144,7 @@ async def unzipper_cb(unzip_bot: Client, query: CallbackQuery):
                     )
 
         # Refreshing Inline keyboard
-        await query.message.edit("`Refreshing ⏳...`")
+        await query.message.edit("`Memperbarui⏳...`")
         rpaths = get_files(path=file_path)
         # There are no files let's die
         if not rpaths:
@@ -152,9 +152,9 @@ async def unzipper_cb(unzip_bot: Client, query: CallbackQuery):
                 shutil.rmtree(f"{Config.DOWNLOAD_LOCATION}/{spl_data[1]}")
             except:
                 pass
-            return await query.message.edit("`I've already sent you those files 😐, Don't ask me to resend 😒!`")
+            return await query.message.edit("`Saya telah mengirim file, jangan meminta saya mengirim ulang`")
         i_e_buttons = await make_keyboard(paths=rpaths, user_id=query.from_user.id, chat_id=query.message.chat.id)
-        await query.message.edit("Select Files to Upload!", reply_markup=i_e_buttons)
+        await query.message.edit("Pilih file untuk di upload!", reply_markup=i_e_buttons)
     
     
     elif query.data.startswith("ext_a"):
@@ -166,16 +166,16 @@ async def unzipper_cb(unzip_bot: Client, query: CallbackQuery):
                 shutil.rmtree(f"{Config.DOWNLOAD_LOCATION}/{spl_data[1]}")
             except:
                 pass
-            return await query.message.edit("`I've already sent you those files 😐, Don't ask me to resend 😒!`")
+            return await query.message.edit("`Saya telah mengirim file anda, jangan meminta saya mengirim ulang`")
         for file in paths:
-            await query.answer("Send that file to you. Please wait!")
+            await query.answer("Saya mengirim Anda file, harap sabar dan tunggu")
             await send_file(unzip_bot=unzip_bot,
                             c_id=spl_data[2],
                             doc_f=file,
                             query=query,
                             full_path=f"{Config.DOWNLOAD_LOCATION}/{spl_data[1]}"
                         )
-        await query.message.edit("**Successfully Uploaded!** \n\n **Join @NexaBotsUpdates ❤️**")
+        await query.message.edit("**Berhasil di upload** \n\n **Support kami melalui do'a shalih kalian #Annajiyah_Media_Center**")
         try:
             shutil.rmtree(f"{Config.DOWNLOAD_LOCATION}/{spl_data[1]}")
         except Exception as e:
@@ -186,7 +186,7 @@ async def unzipper_cb(unzip_bot: Client, query: CallbackQuery):
             shutil.rmtree(f"{Config.DOWNLOAD_LOCATION}/{query.from_user.id}")
             await query.message.edit(Messages.CANCELLED_TXT.format("Process Cancelled"))
         except:
-            return await query.answer("There is nothing to remove lmao!", show_alert=True)
+            return await query.answer("Daulah Islamiyah Baaqiyah", show_alert=True)
     
     elif query.data == "nobully":
-        await query.message.edit("**Ok Ok! I won't delete those files 😂!**")
+        await query.message.edit("**Jangan lupa untuk mendoakan kemenangan Islam dan kaum muslimin, Khususnya Mujahidin Daulah Islamiyah**")
